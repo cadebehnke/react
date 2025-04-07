b7179cf383ad0b9a9b3c6c105e8d4ada98a72b77
Cade Behnke <cadebehnke@Cades-MacBook-Pro-3.local>
2025-04-07 10:31:52 -0400

    add

--- react-01/src/components/ProductCard.jsx
+++ react-01/src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./css/ProductCard.css";
 
+const BASE_URL = "https://housing-backend-ujyb.onrender.com";
+
 function ProductCard({ name, img_name, price, description, rating }) {
   return (
     <div className="product-card">
-      <img src={img_name} alt={name} />
+      <img src={`${BASE_URL}/${img_name}`} alt={name} />
       <div className="details">
         <h3>{name}</h3>
         <p>{description}</p>
